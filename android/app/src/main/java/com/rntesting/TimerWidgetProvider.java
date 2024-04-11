4d6fb5c1e0541171398cd9b9bf52abe9ec4cecde
kimmainsain <kmsid003@gmail.com>
2024-04-11 13:42:24 +0900

    ✨ Feat(android) : Add onUpdate method to TimerWidgetProvider

--- android/app/src/main/java/com/rntesting/TimerWidgetProvider.java
+++ android/app/src/main/java/com/rntesting/TimerWidgetProvider.java
@@ -1,8 +1,18 @@
 package com.rntesting;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
 
 public class TimerWidgetProvider extends AppWidgetProvider {
 
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.timer_widget);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
     
 }
